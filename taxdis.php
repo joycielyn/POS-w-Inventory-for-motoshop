3b375e7b9b08b23e5e8f5af119349931eca2158c
KimEstrella <167540798+kellyclick@users.noreply.github.com>
2026-02-20 15:05:15 +0800

    taxdis.php

--- taxdis.php
+++ taxdis.php
@@ -2,256 +2,208 @@
 include_once 'connectdb.php';
 session_start();
 
+if($_SESSION['useremail']==""){
+    header('location:../index.php');
+}
+
 include_once "header.php";
-if(isset($_POST['btnsave'])) {
-    $sgst = $_POST['txtsgst'];
-    $cgst = $_POST['txtcgst'];
+
+/* ================= SAVE ================= */
+if(isset($_POST['btnsave'])){
+
+    $tax = $_POST['txttax'];
     $discount = $_POST['txtdiscount'];
-  
 
- 
-    if(empty($sgst)) {
-        $_SESSION['status'] = " field is empty";
+    if(empty($tax)){
+        $_SESSION['status'] = "Tax field is empty";
         $_SESSION['status_code'] = "warning";
-    } 
-   
-         else {
-          
-            $insert = $pdo->prepare("INSERT INTO tbl_taxdis (sgst, cgst, discount) VALUES (:sgst, :cgst, :discount)");
-            $insert->bindParam(':sgst', $sgst);
-            $insert->bindParam(':cgst', $cgst);
-            $insert->bindParam(':discount', $discount);
+    }else{
 
-            
-            if ($insert->execute()) {
-                $_SESSION['status'] = "Tax and Discount are added Successfully!";
-                $_SESSION['status_code'] = "success";
-            } else {
-                $_SESSION['status'] = "FAILED!";
-                $_SESSION['status_code'] = "error";
-            }
-        }
-    
-    }
+        $insert = $pdo->prepare("INSERT INTO tbl_taxdis (tax, discount) 
+                                 VALUES (:tax, :discount)");
 
-    if(isset($_POST['btnupdate'])) {
-    
-        $sgst = $_POST['txtsgst'];
-        $cgst = $_POST['txtcgst'];
-        $discount = $_POST['txtdiscount'];
-      
-        $id = $_POST['txtid'];
-        if(empty($sgst)) {
-            $_SESSION['status'] = "SGST field is empty";
-            $_SESSION['status_code'] = "warning";
-        } else {
-            $update = $pdo->prepare("UPDATE tbl_taxdis SET sgst=:sgst, cgst=:cgst, discount=:disc WHERE taxdis_id=".$id);
-            $update->bindParam(':sgst', $sgst);
-            $update->bindParam(':cgst', $cgst);
-            $update->bindParam(':disc', $discount);
-    
-            if ($update->execute()) {
-                $_SESSION['status'] = "Tax and Discount updated Successfully";
-                $_SESSION['status_code'] = "success";
-            } else {
-                $_SESSION['status'] = "Failed to update tax and Discount";
-                $_SESSION['status_code'] = "error";
-            }
+        $insert->bindParam(':tax',$tax);
+        $insert->bindParam(':discount',$discount);
+
+        if($insert->execute()){
+            $_SESSION['status'] = "Tax and Discount Added Successfully";
+            $_SESSION['status_code'] = "success";
+        }else{
+            $_SESSION['status'] = "Failed to Add";
+            $_SESSION['status_code'] = "error";
         }
     }
-    
+}
 
+/* ================= UPDATE ================= */
+if(isset($_POST['btnupdate'])){
 
-// if(isset($_POST['btndelete'])) {
-//     $delete=$pdo->prepare("DELETE FROM tbl_taxdis WHERE taxdis_id=:taxdis_id");
-//     $delete->bindParam(':taxdis_id', $_POST['btndelete']);
-//     if($delete->execute()) {
-//         $_SESSION['status'] = "Deleted successfully";
-//         $_SESSION['status_code'] = "success";
-//     } else {
-//         $_SESSION['status'] = "Deleted not successfully";
-//         $_SESSION['status_code'] = "error";
-//     }
-// }
-?>
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">TAX AND DISCOUNT</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
+    $tax = $_POST['txttax'];
+    $discount = $_POST['txtdiscount'];
+    $id = $_POST['txtid'];
 
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+    if(empty($tax)){
+        $_SESSION['status'] = "Tax field is empty";
+        $_SESSION['status_code'] = "warning";
+    }else{
 
-    <!-- Main content -->
-    <div class="content">
-        <div class="container-fluid">
-            <div class="card card-warning card-outline">
-                <div class="card-header">
-                    <h5 class="m-0">Text  and Discount Form</h5>
-                </div>
-                <div class="card-body">
-                    <form action="" method="post">
-                        <div class="row">
-                            <?php
-                            if(isset($_POST['btnedit'])) {
-                                $select=$pdo->prepare("SELECT * FROM tbl_taxdis WHERE taxdis_id = :catid");
-                                $select->bindParam(':catid', $_POST['btnedit']);
-                                $select->execute();
-                                if($select) {
-                                    $row=$select->fetch(PDO::FETCH_OBJ);
-                                    echo '<div class="col-mg-4">
+        $update = $pdo->prepare("UPDATE tbl_taxdis 
+                                 SET tax=:tax, discount=:discount 
+                                 WHERE taxdis_id=:id");
 
-                                        <div class="form-group">
-                                          
-                                            <input type="hidden" class="form-control" placeholder="Enter Category" value="'.$row->taxdis_id.'" name="txtid">
-                                            
-                                        </div>
+        $update->bindParam(':tax',$tax);
+        $update->bindParam(':discount',$discount);
+        $update->bindParam(':id',$id);
 
-
-                                        <div class="form-group">
-                                        <label for="exampleInputEmail1">SGST(%) </label>
-                                        <input type="text" class="form-control" placeholder="Enter SGST"  value="'.$row->sgst.'" name="txtsgst">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail1">CGST(%)</label>
-                                        <input type="text" class="form-control" placeholder="Enter CGST " value="'.$row->cgst.'"  name="txtcgst">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail1">Discount (%)</label>
-                                        <input type="text" class="form-control" placeholder="Enter Discount"   value="'.$row->discount.'" name="txtdiscount">
-                                    </div>
-
- 
-                                        
-
-                                        
-                                        <div class="card-footer">
-                                            <button type="submit" class="btn btn-info" name="btnupdate">Update</button>
-                                        </div>
-                                    </div>';
-                                }
-                            } else {
-                                echo '<div class="col-mg-4">
-                                        <div class="form-group">
-                                            <label for="exampleInputEmail1">SGST(%) </label>
-                                            <input type="text" class="form-control" placeholder="Enter SGST" name="txtsgst">
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label for="exampleInputEmail1">CGST(%)</label>
-                                            <input type="text" class="form-control" placeholder="Enter CGST " name="txtcgst">
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label for="exampleInputEmail1">Discount (%)</label>
-                                            <input type="text" class="form-control" placeholder="Enter Discount" name="txtdiscount">
-                                        </div>
-
-                                        <div class="card-footer">
-                                            <button type="submit" class="btn btn-warning" name="btnsave">Save</button>
-                                        </div>
-                                    </div>';
-                            }
-                            ?>
-                            <div class="col-md-8">
-                                <table id="table_tax" class="table table-striped table-hover">
-                                    <thead>
-                                        <tr>
-                                            <td>#</td>
-                                            <td>SGST</td>
-                                            <td>CGST</td>
-                                            <td>Discount</td>
-                                            <td>Edit</td>
-                                            <!-- <td>Delete</td> -->
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        $select = $pdo->prepare("SELECT * FROM tbl_taxdis ORDER BY taxdis_id ASC");  
-                                        $select->execute();
-                                        while($row=$select->fetch(PDO::FETCH_OBJ)) {
-                                            echo '
-                                                <tr>
-                                                    <td>'.$row->taxdis_id.'</td>
-                                                    <td>'.$row->sgst.'</td>
-                                                    <td>'.$row->cgst.'</td>
-                                                    <td>'.$row->discount.'</td>
-
-
-                                                    <td>
-                                                        <button type="submit" class="btn btn-primary btn-edit" value="'.$row->taxdis_id.'" name="btnedit">Edit</button>
-                                                    </td>
-                                                
-                                                </tr>';
-                                        }
-                                        ?>
-                                    </tbody>
-                                    <tfoot>
-                                        <tr>
-                                             <td>#</td>
-                                            <td>SGST</td>
-                                            <td>CGST</td>
-                                            <td>Discount</td>
-                                            <td>Edit</td>
-                                            <!-- <td>Delete</td> -->
-                                        </tr>
-                                    </tfoot>
-                                </table>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- /.content -->
-</div>
-<!-- /.content-wrapper -->
-<?php
-include_once "footer.php";
-if(isset($_SESSION['status']) && $_SESSION['status']!='') {
-    echo "<script>
-        Swal.fire({
-            icon: '".$_SESSION['status_code']."',
-            title: '".$_SESSION['status']."'
-        });
-    </script>";
-    unset($_SESSION['status']);
+        if($update->execute()){
+            $_SESSION['status'] = "Updated Successfully";
+            $_SESSION['status_code'] = "success";
+        }else{
+            $_SESSION['status'] = "Update Failed";
+            $_SESSION['status_code'] = "error";
+        }
+    }
 }
 ?>
+
+<!-- Content Wrapper -->
+<div class="content-wrapper">
+
+<div class="content-header">
+<div class="container-fluid">
+<div class="row mb-2">
+<div class="col-sm-6">
+<h1 class="m-0">TAX AND DISCOUNT</h1>
+</div>
+</div>
+</div>
+</div>
+
+<div class="content">
+<div class="container-fluid">
+
+<div class="card card-warning card-outline">
+<div class="card-header">
+<h5 class="m-0">Tax and Discount Form</h5>
+</div>
+
+<div class="card-body">
+<form action="" method="post">
+<div class="row">
+
+<?php
+/* ================= EDIT MODE ================= */
+if(isset($_POST['btnedit'])){
+
+    $select = $pdo->prepare("SELECT * FROM tbl_taxdis WHERE taxdis_id=:id");
+    $select->bindParam(':id',$_POST['btnedit']);
+    $select->execute();
+    $row=$select->fetch(PDO::FETCH_OBJ);
+?>
+
+<div class="col-md-4">
+
+<input type="hidden" name="txtid" value="<?php echo $row->taxdis_id; ?>">
+
+<div class="form-group">
+<label>Tax (%)</label>
+<input type="number" step="0.01" class="form-control" name="txttax" value="<?php echo $row->tax; ?>" required>
+</div>
+
+<div class="form-group">
+<label>Discount (%)</label>
+<input type="number" step="0.01" class="form-control" name="txtdiscount" value="<?php echo $row->discount; ?>">
+</div>
+
+<button type="submit" class="btn btn-info" name="btnupdate">Update</button>
+
+</div>
+
+<?php
+}else{
+?>
+
+<div class="col-md-4">
+
+<div class="form-group">
+<label>Tax (%)</label>
+<input type="number" step="0.01" class="form-control" name="txttax" required>
+</div>
+
+<div class="form-group">
+<label>Discount (%)</label>
+<input type="number" step="0.01" class="form-control" name="txtdiscount">
+</div>
+
+<button type="submit" class="btn btn-warning" name="btnsave">Save</button>
+
+</div>
+
+<?php } ?>
+
+<!-- ================= TABLE ================= -->
+<div class="col-md-8">
+
+<table id="table_tax" class="table table-striped table-hover">
+<thead>
+<tr>
+<td>ID</td>
+<td>Tax (%)</td>
+<td>Discount (%)</td>
+<td>Edit</td>
+</tr>
+</thead>
+
+<tbody>
+
+<?php
+$select=$pdo->prepare("SELECT * FROM tbl_taxdis ORDER BY taxdis_id ASC");
+$select->execute();
+
+while($row=$select->fetch(PDO::FETCH_OBJ)){
+echo '
+<tr>
+<td>'.$row->taxdis_id.'</td>
+<td>'.$row->tax.'</td>
+<td>'.$row->discount.'</td>
+<td>
+<button type="submit" class="btn btn-primary" 
+value="'.$row->taxdis_id.'" name="btnedit">Edit</button>
+</td>
+</tr>';
+}
+?>
+
+</tbody>
+</table>
+
+</div>
+</div>
+</form>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+
+<?php
+include_once "footer.php";
+
+/* ================= ALERT ================= */
+if(isset($_SESSION['status']) && $_SESSION['status']!=''){
+echo "<script>
+Swal.fire({
+icon: '".$_SESSION['status_code']."',
+title: '".$_SESSION['status']."'
+});
+</script>";
+unset($_SESSION['status']);
+}
+?>
+
 <script>
-    $(document).ready(function() {
-        $('.btn-delete').click(function() {
-            var catId = $(this).val();
-            Swal.fire({
-                title: 'Confirmation',
-                text: 'Are you sure you want to delete this? Once deleted, you will not be able to recover this category!',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d63032',
-                confirmButtonText: 'Yes, delete it!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    $('form').append('<input type="hidden" name="btndelete" value="'+catId+'">').submit();
-                }
-            });
-        });
-    });
-</script>
-<script>
-    $(document).ready( function () {
-        $('#table_tax').DataTable();
-    });
+$(document).ready(function () {
+$('#table_tax').DataTable();
+});
 </script>
