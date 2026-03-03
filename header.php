c71481a9929141489021ee6a4f2f21403503a03f
KimEstrella <167540798+kellyclick@users.noreply.github.com>
2026-03-03 16:19:22 +0800

    header.php

--- header.php
+++ header.php
@@ -1,57 +1,25 @@
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | POS INVENTORY SYSTEM</title>
+  <title>POS W INVENTORY SYSTEM </title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
-
-
-  <!-- iCheck for checkboxes and radio inputs -->
-  <link rel="stylesheet" href="../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-
- <!-- Select2 -->  
-  <link rel="stylesheet" href="../plugins/select2/css/select2.min.css">
-  <link rel="stylesheet" href="../plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-
-
-
   <!-- Theme style -->
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
-  <!-- DataTables -->
-  <link rel="stylesheet" href="../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-  <link rel="stylesheet" href="../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
-
-
-<!--SweetAlert2 -->
-<link rel="stylesheet" href="../plugins/sweetalert2/sweetalert2.min.css">
-
-
 </head>
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition sidebar-mini sidebar-collapse">
 <div class="wrapper">
 
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
+  <nav class="main-header navbar navbar-expand navbar-black navbar-dark">
     <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-         
       </li>
     </ul>
 
@@ -62,6 +30,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <a class="nav-link" data-widget="navbar-search" href="#" role="button">
           <i class="fas fa-search"></i>
         </a>
+        
         <div class="navbar-search-block">
           <form class="form-inline">
             <div class="input-group input-group-sm">
@@ -78,112 +47,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </form>
         </div>
       </li>
-
-      <!-- Messages Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-comments"></i>
-          <span class="badge badge-danger navbar-badge">3</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="../dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Brad Diesel
-                  <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">Call me whenever you can...</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="../dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  John Pierce
-                  <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">I got your message bro</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="../dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Nora Silvester
-                  <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">The subject goes here</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-        </div>
-      </li>
-      <!-- Notifications Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-bell"></i>
-          <span class="badge badge-warning navbar-badge">15</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-header">15 Notifications</span>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i> 4 new messages
-            <span class="float-right text-muted text-sm">3 mins</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-users mr-2"></i> 8 friend requests
-            <span class="float-right text-muted text-sm">12 hours</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-file mr-2"></i> 3 new reports
-            <span class="float-right text-muted text-sm">2 days</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
     </ul>
   </nav>
-  <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <aside class="main-sidebar bg-dark elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="profile.php" class="brand-link">
       <img src="../dist/img/logo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">POS INVENTORY</span>
+      <span class="brand-text font-weight-light">MOTORSHOP</span>
     </a>
 
     <!-- Sidebar -->
@@ -194,50 +66,22 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="../dist/img/logo.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block"><?php echo $_SESSION['username']; ?></a>
-        </div>
-      </div>
-
-      <!-- SidebarSearch Form -->
-      <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
-          </div>
+          <!-- Make the username text white -->
+          <a href="profile.php" class="d-block" style="color: #fff;"><?php echo $_SESSION['username']; ?></a>
         </div>
       </div>
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
           
           <li class="nav-item">
             <a href="dashboard.php" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-                
-              </p>
+              <p>Dashboard</p>
             </a>
           </li>
-
-          <li class="nav-item">
-            <a href="category.php" class="nav-link">
-              <i class="nav-icon fas fa-table"></i>
-              <p>
-                Category
-                
-              </p>
-            </a>
-          </li>
-
-
-          <li class="nav-item">
+                    <li class="nav-item">
             <a href="supplier.php" class="nav-link">
               <i class="nav-icon fas fa-table"></i>
               <p>
@@ -245,20 +89,22 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 
               </p>
             </a>
-          </li>
+          </li> 
 
           <li class="nav-item">
-            <a href="productlist.php" class="nav-link">
-              <i class="nav-icon fas fa-list"></i>
-              <p>
-                Product List
-               
-              </p>
+            <a href="productlist.php" class="nav-link"> 
+              <i class="nav-icon fas fa-edit"></i>
+              <p>Product List</p>
             </a>
           </li>
 
-          </li>
           <li class="nav-item">
+            <a href="orderlist.php" class="nav-link">
+              <i class="nav-icon fas fa-list"></i>
+              <p>Order List</p>
+            </a>
+          </li>
+                    <li class="nav-item">
             <a href="pos.php" class="nav-link">
               <i class="nav-icon fas fa-book"></i>
               <p>
@@ -267,34 +113,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </p>
             </a>
           </li>
-
-
-
-          </li>
-          <li class="nav-item">
-            <a href="orderlist.php" class="nav-link">
-              <i class="nav-icon fas fa-list"></i>
-              <p>
-                Order List
-               
-              </p>
-            </a>
-          </li>
-
-
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-chart-pie"></i>
-              <p>
-                Sales Report
-               
-              </p>
-            </a>
-          </li>
-
-          </li>
-          <li class="nav-item">
+                    <li class="nav-item">
             <a href="taxdis.php" class="nav-link">
               <i class="nav-icon fas fa-calculator"></i>
               <p>
@@ -303,8 +122,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </p>
             </a>
           </li>
-
-
+          <li class="nav-item">
+            <a href="salesreport.php" class="nav-link">
+              <i class="nav-icon fas fa-chart-pie"></i>
+              <p>Sales Report</p>
+            </a>
           </li>
           <li class="nav-item">
             <a href="registration.php" class="nav-link">
@@ -316,28 +138,104 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
           </li>
 
-          </li>
-          <li class="nav-item">
-            <a href="changepassword.php" class="nav-link">
-              <i class="nav-icon fas fa-user-lock"></i>
-              <p>
-                Change Password
-               
-              </p>
-            </a>
-          </li>
+         <!-- Utilities Dropdown Start -->
+<li class="nav-item has-treeview">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-tools"></i>
+    <p>
+      Utilities
+      <i class="right fas fa-angle-left"></i>
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="changepassword.php" class="nav-link">
+        <i class="nav-icon fas fa-user-lock"></i>
+        <p>Change Password</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="archive.php" class="nav-link">
+        <i class="nav-icon fas fa-archive"></i>
+        <p>Archived Products</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="trashbin.php" class="nav-link">
+        <i class="nav-icon fas fa-trash"></i>
+        <p>Trash Bin</p>
+      </a>
+    </li>
+  </ul>
+</li>
 
           <li class="nav-item">
             <a href="logout.php" class="nav-link">
-              <i class="nav-icon fas fa-sign-out-alt"></i>
-              <p>
-                LogOut
-              </p>
+              <i class="nav-icon fas fa-th"></i>
+              <p>Logout</p>
             </a>
           </li>
+
         </ul>
       </nav>
-      <!-- /.sidebar-menu -->
     </div>
-    <!-- /.sidebar -->
   </aside>
+
+  <style>
+
+    .main-header.navbar-black {
+      background-color: #000 !important; /* Set the navbar background color to black */
+    }
+
+    /* Custom Sidebar Color */
+    .main-sidebar {
+      background-color: #000 !important; /* Set the sidebar color to black */
+    }
+
+    .sidebar-dark-primary {
+      background-color: #000 !important; /* Ensure dark sidebar is black */
+    }
+
+    /* Make the brand text (Hubek Cafe) white */
+    .sidebar .brand-text {
+      color: #fff !important; /* White color for brand text */
+    }
+
+    /* Make the sidebar search input text white */
+    .sidebar .form-control-sidebar {
+      background-color: #333 !important;  /* Dark background for search box */
+      color: #fff !important;              /* White text for the input */
+      border: 1px solid #444 !important;  /* Optional border styling */
+    }
+
+    .sidebar .form-control-sidebar:focus {
+      background-color: #444 !important;  /* Darker background on focus */
+      color: #fff !important;             /* White text when focused */
+      box-shadow: none;                   /* Remove focus shadow */
+    }
+
+    /* Make the search button white */
+    .sidebar .btn-sidebar {
+      background-color: #444 !important;  /* Dark background for search button */
+      color: #fff !important;             /* White icon color */
+    }
+
+    /* Prevent blue color on focus/hover for sidebar links */
+    .sidebar .nav-link {
+      color: #fff !important; /* Ensure all sidebar links are white */
+    }
+
+    .sidebar .nav-link:hover,
+    .sidebar .nav-link:focus {
+      color: #fff !important; /* Keep text white even on hover/focus */
+      background-color: #444 !important; /* Optional: Change background color on hover */
+    }
+
+
+    
+  </style>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+</body>
+</html>
